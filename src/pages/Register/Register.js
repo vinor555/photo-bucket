75c8180d41de24e8621a5c794318be80d22fa226
aleesan7 <ka_sanchezsamayoa@outlook.com>
2023-03-20 13:53:04 -0600

    Modificaciones a pagina de registro

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { makeStyles, Paper, TextField, Button } from "@material-ui/core";
 import axios from "axios";
 import { API_URL } from "../../utils/Constants";
+import './Register.css'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,6 +40,13 @@ function Register() {
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
 
+  const [postImage, setPostImage] = useState({
+    image: "",
+    idImage: ""
+  });
+
+const [file, setFile] = useState();
+
   const handleRegister = () => {
     if (userName !== "" && userName2 !== "") {
       if (password !== "" && password === password2) {
@@ -69,13 +77,53 @@ function Register() {
       .then((response) => {
         console.log(response.data.msg);
         if (response.data.msg === "Usuario registrado exitosamente!") {
-          window.location.href = "/";
+
+          const body2 = {
+            id : postImage.idImage,
+            uType : 'perfil',
+            album : 'perfil',
+            user : userName,
+            photo : postImage.image
+          }
+
+          axios.post(`${API_URL}/images/upload`, body2).then((response2)=>{
+            console.log(response2.data.msg);
+            
+            if(response2.data.message==='Se agrego la imagen exitosamente'){
+              window.location.href = "/";
+            }
+          }).catch((error) => {
+            console.error(error);
+          });
         }
       })
       .catch((error) => {
         console.error(error);
       });
   }
+
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        resolve(fileReader.result.split(',')[1]);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const handleFileUpload = async (e) => {
+    const file = e.target.files[0];
+    const name = file.name;
+
+    const base64 = await convertToBase64(file);
+
+    setPostImage({ ...postImage, image: base64, idImage :  name});
+    setFile(URL.createObjectURL(file));
+  };
 
   return (
     <div className={classes.root}>
@@ -98,7 +146,7 @@ function Register() {
             required
             fullWidth
             id="email"
-            label="Username 2"
+            label="Nombre Completo"
             name="email"
             autoComplete="email"
             onChange={(event) => setUsername2(event.target.value)}
@@ -128,6 +176,21 @@ function Register() {
             autoComplete="new-password"
             onChange={(event) => setPassword2(event.target.value)}
           />
+
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="fileUploader"
+            name="fileUploader"
+            autoFocus
+            type='file'
+            onChange={(e) => handleFileUpload(e)}
+          />
+          
+          <img src={file} />
+
           <Button
             //type="submit"
             fullWidth
